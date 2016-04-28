8db9803bea75555b7b0153a0b48330b12950e25e
thabongakane <mrngakane>
2016-04-28 11:41:40 +0200

    Revised File

---
+++ InTheClub.py
@@ -0,0 +1,34 @@
+import sys
+import queue
+
+#def main()
+#def gant_entrance
+#    pass
+
+#class partyLife (0)
+
+#def ppl_in_queue
+
+# getNumeric(prompt):
+
+#    while True:
+#        response = input(prompt)
+#        try:
+#            return int(response)
+#        except ValueError:
+#                print ("Please enter you age:    " )
+#age = getNumeric("What is your age")
+#print (age)
+
+
+#def sortque
+
+ppl_in_queue = []
+tot_num_of_people = int(input("How many people are awaiting entry....?   "))
+for ppl_counter in range(0, tot_num_of_people):
+    legal_age = input("Enter every individuals age:    ")
+    ppl_in_queue.append(legal_age)
+    sorted(ppl_in_queue.reverse)
+    print (ppl_in_queue)
+
+#def solveforx
